c0c2450bfc87e86f99704daec17041c9739e3663
Shantanu <thegeek.004@gmail.com>
2019-02-16 18:07:27 +0530

    extract image metadata in create_dataset.py

--- dataset/create_dataset.py
+++ dataset/create_dataset.py
@@ -2,6 +2,48 @@ import os
 import argparse
 
 
+def load_images_data(img_ids, images_data, label):
+    img_filenames = []
+    img_categories = []
+    img_captions = []
+    for img_id in img_ids:
+        img_filenames.append(images_data[img_id]['file_name'])
+        img_categories.append(images_data[img_id]['categories'])
+        img_captions.append(images_data[img_id]['captions'])
+    
+    if label == 'categories':
+        return (img_filenames, img_categories)
+    return (img_filenames, img_captions)
+
+
+def load_coco(input_path, label, split_train=0.8, split_val=0.19):
+    """ Load coco dataset
+        :params input_path: path to the parsed coco file
+        :params split: split between training and validation dataset
+    """
+    with open(input_path, 'rb') as file:
+        coco_raw = pickle.load(file)
+    images_data = coco_raw['images_data']
+    category_id = coco_raw['category_id']
+    id_category = coco_raw['id_category']
+    
+    # split dataset
+    img_ids = list(images_data.keys())
+    split_idx_train = int(len(img_ids) * split_train)
+    split_idx_val = int(len(img_ids) * split_val)
+    random.shuffle(img_ids)
+    img_ids_train = img_ids[:split_idx_train]
+    img_ids_val = img_ids[split_idx_train:split_idx_train+split_idx_val]
+    img_ids_test = img_ids[split_idx_train+split_idx_val:]
+    
+    # load dataset
+    train_data = load_images_data(img_ids_train, images_data, label)  # training dataset
+    val_data = load_images_data(img_ids_val, images_data, label)  # validation dataset
+    test_data = load_images_data(img_ids_test, images_data, label)  # test dataset
+    
+    return train_data, val_data, test_data, category_id, id_category
+
+
 def main(args):
     pass
 
@@ -16,19 +58,18 @@ if __name__ == '__main__':
         '--raw', default=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'coco_raw.pickle'),
         help='Path to the simplified raw coco file'
     )
-    parser.add_argument('--categories', action='store_true', help='Encode image categories')
-    parser.add_argument('--captions', action='store_true', help='Encode image captions')
-    parser.add_argument('--train_split', default=0.8, help='Training data split')
-    parser.add_argument('--val_split', default=0.19, help='Validation data split')
+    parser.add_argument('--label', choices=['categories', 'captions'], help='Type of label vector to create')
+    parser.add_argument('--split_train', default=0.8, help='Training data split')
+    parser.add_argument('--split_val', default=0.19, help='Validation data split')
     args = parser.parse_args()
 
-    if args.train_split <= 0 or args.train_split >= 1:
-        print('Value of train_split should be between 0 and 1')
-    elif args.val_split <= 0 or args.val_split >= 1:
-        print('Value of val_split should be between 0 and 1')
-    elif args.train_split <= args.val_split:
-        print('train_split should be greater than val_split')
-    elif args.train_split + args.val_split >= 1:
+    if args.split_train <= 0 or args.split_train >= 1:
+        print('Value of split_train should be between 0 and 1')
+    elif args.split_val <= 0 or args.split_val >= 1:
+        print('Value of split_val should be between 0 and 1')
+    elif args.split_train <= args.split_val:
+        print('split_train should be greater than split_val')
+    elif args.split_train + args.split_val >= 1:
         print('Please enter a valid split')
     else:
         main(args)

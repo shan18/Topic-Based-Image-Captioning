b08168bc4b5db9d6694a19dda9c20896c17f1307
Shantanu <thegeek.004@gmail.com>
2019-04-07 18:23:58 +0530

    fix process feature dataset

--- dataset/process_feature_dataset.py
+++ dataset/process_feature_dataset.py
@@ -17,13 +17,7 @@ def load_data(data_type, data_dir):
     vgg_feature_cache_path = os.path.join(
         data_dir, 'vgg_features_{}.h5'.format(data_type)
     )
-    topics_cache_path = os.path.join(
-        data_dir, 'topics_{}.pkl'.format(data_type)
-    )
 
-    if os.path.exists(topics_cache_path):
-        with open(topics_cache_path, mode='rb') as file:
-            topics = pickle.load(file)
     if os.path.exists(vgg_feature_cache_path):
         vgg_feature_file = h5py.File(vgg_feature_cache_path, 'r')
         vgg_feature_obj = vgg_feature_file['feature_values']
@@ -31,7 +25,22 @@ def load_data(data_type, data_dir):
         sys.exit('processed {} data does not exist.'.format(data_type))
 
     print('{} data loaded from cache-file.'.format(data_type))
-    return vgg_feature_file, vgg_feature_obj, topics
+    return vgg_feature_file, vgg_feature_obj
+
+
+def get_topic_shape(data_type, data_dir):
+    # Path for the cache-file.
+    topics_cache_path = os.path.join(
+        data_dir, 'topics_{}.pkl'.format(data_type)
+    )
+
+    if os.path.exists(topics_cache_path):
+        with open(topics_cache_path, mode='rb') as file:
+            topics = pickle.load(file)
+    else:
+        sys.exit('processed {} data does not exist.'.format(data_type))
+    
+    return topics.shape[1]
 
 
 def process_data(feature_model, vgg_features, save_dir, data_type, batch_size):
@@ -68,19 +77,20 @@ def process_data(feature_model, vgg_features, save_dir, data_type, batch_size):
 
 def main(args):
     # Load pre-processed data
-    vgg_feature_file_train, vgg_features_train, topics_train = load_data(
+    vgg_feature_file_train, vgg_features_train = load_data(
         'train', args.data
     )
-    vgg_feature_file_val, vgg_features_val, _ = load_data(
+    vgg_feature_file_val, vgg_features_val = load_data(
         'val', args.data
     )
-    vgg_feature_file_test, vgg_features_test, _ = load_data(
+    vgg_feature_file_test, vgg_features_test = load_data(
         'test', args.data
     )
     print('\nFeatures shape:', vgg_features_train.shape)
     
     # Load pre-trained feature model
-    feature_model = load_feature_model(vgg_features_train.shape[1:], topics_train.shape[1], args.weights)
+    num_topics = get_topic_shape('train', args.data)
+    feature_model = load_feature_model(vgg_features_train.shape[1:], num_topics, args.weights)
 
     # Generate and save dataset
     process_data(  # training data
